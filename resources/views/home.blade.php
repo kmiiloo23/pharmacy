1111f0a55c8eb1db91d32529eff71a821032d179
kmiiloo23 <kmilomb23dic@gmail.com>
2024-05-01 18:11:45 -0500

    Añadir sistema de registracion e inicia de sesion.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.plantillas')
+
+@section('title', 'Home')
+
+@section('content')
+    <h1>Bienvenido a la pagina principal</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomeController::class);
+
+// Show Register/Create Form
+Route::get('/register', [UserController::class, 'create'])->middleware('guest');
+
+// Create New User
+Route::post('/users', [UserController::class, 'store']);
+
+// Log User Out
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
+
+// Show Login Form
+Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest');
+
+// Log In User
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
